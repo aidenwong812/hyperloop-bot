6e2591aa64b87231726217acf75a3ddf563fd6a3
aidenwong812 <wongaiden812@gmail.com>
2024-08-24 03:53:53 +0800

    remove unnecessary files

--- src/events/volume.event/keyboards.ts
+++
@@ -1,9 +0,0 @@
-export const boostVolumeKeyboard = () => [
-  [{ text: '🚀 Fast: 6H', callback_data: 'boostVolume fastMode' }],
-  [{ text: '🚈 Normal: 24H', callback_data: 'boostVolume normalMode' }],
-  [{ text: '🎮 Learn more', callback_data: 'learnMore' }],
-];
-
-export const boostVolumeModeKeyboard = () => [
-  [{ text: '🔙 Return', callback_data: 'return' }],
-];
